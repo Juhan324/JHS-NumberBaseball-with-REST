a079a971d90a457b3699de09b7681bc6db110ffd
Juhan324 <77876969+Juhan324@users.noreply.github.com>
2022-08-10 11:51:28 +0900

    Create Project
    숫자 야구 REST 프로젝트 시작

---
+++ numberbaseball/src/main/java/com/jhs/numberbaseball/NumberbaseballApplication.java
@@ -0,0 +1,13 @@
+package com.jhs.numberbaseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberbaseballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberbaseballApplication.class, args);
+	}
+
+}
---
+++ numberbaseball/src/test/java/com/jhs/numberbaseball/NumberbaseballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhs.numberbaseball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberbaseballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
